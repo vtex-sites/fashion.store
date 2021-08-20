88ecdda2c512c7a5337401471de6af4691d9ee1b
Tiago Gimenes <tlgimenes@gmail.com>
2021-08-20 10:46:18 -0300

    add drawer

--- src/components/cart/CartSidebar/CartSidebar.tsx
+++ src/components/cart/CartSidebar/CartSidebar.tsx
@@ -1,26 +1,31 @@
-import { useCart } from '@vtex/store-sdk'
+import { useCart, useGlobalUIState } from '@vtex/store-sdk'
 import React from 'react'
+import Drawer from 'src/components/ui/Drawer'
 
+import * as styles from './CartSidebar.module.css'
 import CartItem from '../CartItem'
 
 function CartSidebar() {
+  const { closeMinicart } = useGlobalUIState()
   const { items, total, subTotal, totalUniqueItems, totalItems } = useCart()
 
   return (
-    <div>
-      <div>Cart Item Detais</div>
+    <Drawer isOpen onClose={closeMinicart} variant="cart" placement="right">
+      <div className={styles.container}>
+        <div>Cart Item Detais</div>
 
-      {items.map((item) => (
-        <CartItem key={item.id} item={item} />
-      ))}
+        {items.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
 
-      <div>Cart Summary</div>
+        <div>Cart Summary</div>
 
-      <div>uniqueItems: {totalUniqueItems}</div>
-      <div>items: {totalItems}</div>
-      <div>subTotal: {subTotal}</div>
-      <div>total: {total}</div>
-    </div>
+        <div>uniqueItems: {totalUniqueItems}</div>
+        <div>items: {totalItems}</div>
+        <div>subTotal: {subTotal}</div>
+        <div>total: {total}</div>
+      </div>
+    </Drawer>
   )
 }
 
--- src/components/common/Navlinks/Navlinks.tsx
+++ src/components/common/Navlinks/Navlinks.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Heart, User, ShoppingCart } from 'react-feather'
+import { useGlobalUIState } from '@vtex/store-sdk'
 
 import * as styles from './Navlinks.module.css'
 
 function Navlinks() {
+  const { openMinicart } = useGlobalUIState()
+
   return (
     <nav className={styles.root}>
       <span className={styles.languageSelector}>UK</span>
       <Heart className={`${styles.navlink} ${styles.wishListNav}`} />
       <User className={`${styles.navlink} ${styles.userNav}`} />
-      <ShoppingCart className={styles.navlink} />
+      <button className={styles.cart} onClick={openMinicart}>
+        <ShoppingCart className={styles.navlink} />
+      </button>
     </nav>
   )
 }
