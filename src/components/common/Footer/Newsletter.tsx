1376ba4c007e8b035db79a139f0eee360e54c02c
Larícia Mota <laricia.mota@vtex.com.br>
2021-08-20 11:03:38 -0300

    Add label to form

--- src/components/common/Footer/Newsletter.tsx
+++ src/components/common/Footer/Newsletter.tsx
@@ -7,12 +7,12 @@ import * as styles from './Footer.module.css'
 export const Newsletter = (props: FormHTMLAttributes<HTMLFormElement>) => (
   <form {...props}>
     <div className={styles.form}>
-      Join our newsletter
+      <label htmlFor="newsletter-input">Join our newsletter</label>
       <div className={styles.subtitle}>
         Sign up for email updates on the last collections, campaings and videos
       </div>
       <div className={styles.input}>
-        <Input placeholder="ENTER YOUR EMAIL" />
+        <Input placeholder="ENTER YOUR EMAIL" id="newsletter-input" />
         <Button>Sign up</Button>
       </div>
     </div>
