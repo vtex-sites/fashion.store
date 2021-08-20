cc83de5f111f6891323882e0c9b6e71beaf4d3f1
Larícia Mota <laricia.mota@vtex.com.br>
2021-08-20 11:35:56 -0300

    Fix newsletter id

--- src/components/common/Footer/Newsletter.tsx
+++ src/components/common/Footer/Newsletter.tsx
@@ -4,17 +4,22 @@ import { Button, Input } from '@vtex/store-ui'
 
 import * as styles from './Footer.module.css'
 
-export const Newsletter = (props: FormHTMLAttributes<HTMLFormElement>) => (
-  <form {...props}>
-    <div className={styles.form}>
-      <label htmlFor="newsletter-input">Join our newsletter</label>
-      <div className={styles.subtitle}>
-        Sign up for email updates on the last collections, campaings and videos
+export const Newsletter = (props: FormHTMLAttributes<HTMLFormElement>) => {
+  const { id, ...prop } = props
+
+  return (
+    <form {...prop}>
+      <div className={styles.form}>
+        <label htmlFor={id}>Join our newsletter</label>
+        <div className={styles.subtitle}>
+          Sign up for email updates on the last collections, campaings and
+          videos
+        </div>
+        <div className={styles.input}>
+          <Input placeholder="ENTER YOUR EMAIL" id={id} />
+          <Button>Sign up</Button>
+        </div>
       </div>
-      <div className={styles.input}>
-        <Input placeholder="ENTER YOUR EMAIL" id="newsletter-input" />
-        <Button>Sign up</Button>
-      </div>
-    </div>
-  </form>
-)
+    </form>
+  )
+}
