1c860667c894fa0f4f159cc61841568fa939e8ba
Maíra Bello <maira.bello@vtex.com.br>
2021-08-20 09:57:05 -0300

    Remove wishlist link
    Lighthouse is complaining that the wishlist page doesn't exist yet

--- src/components/common/Navlinks/Navlinks.tsx
+++ src/components/common/Navlinks/Navlinks.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Heart, User, ShoppingCart } from 'react-feather'
-import { Link } from 'gatsby'
 
 import * as styles from './Navlinks.module.css'
 
@@ -8,9 +7,7 @@ function Navlinks() {
   return (
     <nav className={styles.root}>
       <span className={styles.languageSelector}>UK</span>
-      <Link to="/wishlist" aria-label="Wishlist">
-        <Heart className={`${styles.navlink} ${styles.wishListNav}`} />
-      </Link>
+      <Heart className={`${styles.navlink} ${styles.wishListNav}`} />
       <User className={`${styles.navlink} ${styles.userNav}`} />
       <ShoppingCart className={styles.navlink} />
     </nav>
