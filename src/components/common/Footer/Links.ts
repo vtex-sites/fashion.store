b139291883d6636f6589e1ff59225018f8874961
Larícia Mota <laricia.mota@vtex.com.br>
2021-08-20 10:18:46 -0300

    Add footer

---
+++ src/components/common/Footer/Links.ts
@@ -0,0 +1,26 @@
+export const aboutUs = [
+  { text: 'About Us', to: '/' },
+  { text: 'About VTEX', to: '/' },
+  { text: 'Careers', to: '/' },
+  { text: 'Legal', to: '/' },
+  { text: 'Privacy & Cookies', to: '/' },
+  { text: 'Corporate Information', to: '/' },
+]
+
+export const customerServices = [
+  { text: 'Customer Services', to: '/' },
+  { text: 'Contact Us', to: '/' },
+  { text: 'Payment', to: '/' },
+  { text: 'FAQs', to: '/' },
+  { text: 'Product After Care', to: '/' },
+  { text: 'Gift Cards', to: '/' },
+]
+
+export const needHelp = [
+  { text: 'Need Help', to: '/' },
+  { text: 'Contact Us', to: '/' },
+  { text: 'Shipping Services', to: '/' },
+  { text: 'Payment Options', to: '/' },
+  { text: 'Returns & Exchanges', to: '/' },
+  { text: 'Product Care', to: '/' },
+]
